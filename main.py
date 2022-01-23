b8657ab59a2df9503cb531671a85d9b0ab4055da
Mikolaj <mikolaj.kubista@gmail.com>
2022-01-23 19:17:08 +0100

    defined inputs to trigger command

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+from multiprocessing.dummy import Array
 import typer
+from typing import List
 
 app = typer.Typer()
 
-@app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
+@app.callback()
+def callback():
+    """
+    My handy cli app.
+    For now it has triggerbuild command 
+    which tags git repo its ran in.
+    """
 
-
 @app.command()
-def triggerbuild(tag: str):
-    typer.echo(f"Entered desired tag {tag}")
+def triggerbuild(services: List[str], env: str = typer.Option(...)):
+    typer.echo("Setting git tags to trigger deployment(s)")
+    typer.echo(f"Deploying to {env} environment.")
+    typer.echo("Deploying following microservices:")
+    for x in services:
+        typer.echo(f"{x}")
 
 
 if __name__ == "__main__":
