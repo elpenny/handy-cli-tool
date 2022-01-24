5b824e2660e59ec918eb35605e65af0dd5545578
Mikolaj <mikolaj.kubista@gmail.com>
2022-01-25 00:44:43 +0100

    better interface for tag

--- src/command_groups/services.py
+++ src/command_groups/services.py
@@ -1,18 +1,29 @@
+from .text import help_text
 import typer
 from typing import List
 
+
 app = typer.Typer()
 
-# This should be subcommand to command "services"
 # Planned features: 
 # option to analyze repo and output warning if not all affected services are going to be tagged
 # dry-run
 # services and envs validation
 # progress bars
 # hints when input has errors
+
 @app.command()
-def tag(services: List[str], env: str = typer.Option(...)):
+def tag(services: List[str] = typer.Argument(..., help=help_text["tag_services"]), 
+        env: str = typer.Option(..., "--env", "-e", help=help_text["tag_env"]),
+        noValidation: bool = typer.Option(False, "--no-validation", "-v", help=help_text["tag_validation"])):
+    """
+    This command lets you tag current repository to trigger deployments.
+
+    """
+    if noValidation:
+        typer.echo("Changed services validation is turned off.")
     typer.echo("Setting git tags to trigger deployment(s)")
+    
     if 'all' in services:
         typer.echo(f"Deploying all services to {env} environment")
     else:
@@ -23,7 +34,7 @@ def tag(services: List[str], env: str = typer.Option(...)):
 
 @app.command()
 def list():
-    typer.echo("Listing services in this repository")
+    typer.echo("Listing deployable services and possible environment targets for this repository")
 
 @app.command()
 def verify():
---
+++ src/command_groups/text.py
@@ -0,0 +1,5 @@
+help_text = {
+    "tag_services": "Names of services to tag, 'all' can be used as substitute",
+    "tag_env": "Specify deployment from the list",
+    "tag_validation": "Disable changed services validation"
+}
